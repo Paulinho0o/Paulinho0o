5c6a794589f36c4e5b2eede628f26353923a00b5
Paulinho0o <149738972+Paulinho0o@users.noreply.github.com>
2023-11-02 21:53:23 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('51 é PINGA!!!'),
+    ),
+    body: Center(
+      child: Text('Vai CORINTHIANS', style: TextStyle(
+        color: Colors.deepOrange,
+        fontSize: 25.0,
+        fontWeight: FontWeight.bold
+      ),),
+    ), 
+  )
+  ));
+}
